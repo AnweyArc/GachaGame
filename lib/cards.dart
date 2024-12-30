83e33344c2e1e085eeddf474df0abb231dd8e7ce
anwey2002 <anwey2002@gmail.com>
2024-12-30 06:27:43 +0000

    card list changed to cart rarity list

--- lib/cards.dart
+++ lib/cards.dart
@@ -22,68 +22,59 @@ class CardModel {
 }
 
 // Example card instances
-List<CardModel> cardList = [
+List<CardModel> cardRarityList = [
   CardModel(
     rarity: 'Common',
     cost: 50,
     luckValue: 1,
     currencyValue: 10,
-    specialEffect: 'No special effect',
   ),
   CardModel(
     rarity: 'Normal',
     cost: 100,
     luckValue: 2,
     currencyValue: 20,
-    specialEffect: 'Increase drop rate slightly',
   ),
   CardModel(
     rarity: 'Rare',
     cost: 200,
     luckValue: 5,
     currencyValue: 50,
-    specialEffect: 'Increase luck by 10%',
   ),
   CardModel(
     rarity: 'Epic',
     cost: 500,
     luckValue: 10,
     currencyValue: 100,
-    specialEffect: 'Increases summon chances for higher rarity cards',
   ),
   CardModel(
     rarity: 'Super Rare',
     cost: 1000,
     luckValue: 20,
     currencyValue: 200,
-    specialEffect: 'Double currency value from summons',
   ),
   CardModel(
     rarity: 'Ultra Rare',
     cost: 2000,
     luckValue: 30,
     currencyValue: 500,
-    specialEffect: 'Boosts chances of summoning Secret Rarity cards',
   ),
   CardModel(
     rarity: 'Ultimate',
     cost: 5000,
     luckValue: 50,
     currencyValue: 1000,
-    specialEffect: 'Grants the ability to summon Godly cards',
   ),
   CardModel(
     rarity: 'Secret Rarity',
     cost: 10000,
     luckValue: 100,
     currencyValue: 5000,
-    specialEffect: 'Special effect grants 100% luck for next summon',
   ),
   CardModel(
     rarity: 'Godly',
     cost: 50000,
     luckValue: 200,
     currencyValue: 10000,
-    specialEffect: 'Instant win, doubles all current currency',
   ),
 ];
